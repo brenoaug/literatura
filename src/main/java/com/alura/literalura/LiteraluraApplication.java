cba79ddd3230df00f5aa7bdafb0cdb9f6ab2248b
breno augusto <breaugustocp@outlook.com>
2026-02-02 11:59:37 -0300

    configurações iniciais do PostgreSQL e implementação da classe ConsumoApi para obter dados de livros

--- src/main/java/com/alura/literalura/LiteraluraApplication.java
+++ src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -1,13 +1,22 @@
 package com.alura.literalura;
 
+import com.alura.literalura.services.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDadosPorLivro();
+		System.out.println(json);
+	}
 }
